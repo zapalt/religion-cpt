231388a393cc1ae6641386967bfee43187ed459f
FrostDev <zapex789@gmail.com>
2023-06-20 02:37:30 -0400

    again

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -30,6 +30,8 @@ function Home() {
                     </div>
                 </div>
             </div>
+
+            <div class="flex flex-col w-full items-center">
             <Pane>
                 <h1 class="font-Unbounded font-bold text-2xl">What is Nostra Aetate?</h1>
                 <div class="pt-4"/>
@@ -46,34 +48,34 @@ function Home() {
                 <p>
                 </p>
             </Pane>
-            <Quiz />
-            <div class="p-10 pt-0 gap-10 flex flex-row">
-                <div>
-                    <p>
-                    Images sourced from:
-                    </p>
-                    <ul class="list-disc list-inside">
-                        <li>
-                            https://www.pexels.com
-                        </li>
-                        <li>
-                            https://commons.wikimedia.org
-                        </li>
-                        <li>
-                            https://unsplash.com/
-                        </li>
-                    </ul>
-                </div>
-                <div class="grow">
-                    <button type="button" class="w-full hs-collapse-toggle py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800" id="hs-basic-collapse" data-hs-collapse="#hs-basic-collapse-heading">
-                        Works Cited
-                        <svg class="hs-collapse-open:rotate-180 w-2.5 h-2.5 text-white" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
-                        </svg>
-                    </button>
-                    <div id="hs-basic-collapse-heading" class="hs-collapse hidden w-full overflow-hidden transition-[height] duration-300" aria-labelledby="hs-basic-collapse">
-                        <div class="mt-5">
-                            <object class="" width="100%" height="500" data={wc} type="application/pdf"></object>
+                <div class="p-10 pt-0 gap-10 flex flex-row w-[70rem]">
+                    <div>
+                        <p>
+                        Images sourced from:
+                        </p>
+                        <ul class="list-disc list-inside">
+                            <li>
+                                https://www.pexels.com
+                            </li>
+                            <li>
+                                https://commons.wikimedia.org
+                            </li>
+                            <li>
+                                https://unsplash.com/
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="grow">
+                        <button type="button" class="w-full hs-collapse-toggle py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800" id="hs-basic-collapse" data-hs-collapse="#hs-basic-collapse-heading ">
+                            Works Cited
+                            <svg class="hs-collapse-open:rotate-180 w-2.5 h-2.5 text-white" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
+                            </svg>
+                        </button>
+                        <div id="hs-basic-collapse-heading" class="hs-collapse hidden w-full overflow-hidden transition-[height] duration-300" aria-labelledby="hs-basic-collapse">
+                            <div class="mt-5">
+                                <object class="" width="100%" height="500" data={wc} type="application/pdf"></object>
+                            </div>
                         </div>
                     </div>
                 </div>
